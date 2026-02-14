13c106c9cc00be51174085492ba970f56e4f89e5
gianogli <gianogli@tiscali.it>
2026-02-14 22:16:47 +0100

    Revert "sdm660-common: Use libprotobuf-cpp-{lite,full}-21.7 from hardware/lineage/compat"
    This reverts commit 2cc169d5df3572e0ada76a89831a8cb809994d1f.

--- sdm660-common-vendor.mk
+++ sdm660-common-vendor.mk
@@ -352,6 +352,7 @@ PRODUCT_PACKAGES += \
     libpdmapper \
     libpdnotifier \
     libperipheral_client \
+    libprotobuf-cpp-full-21.7 \
     libqcc_file_agent \
     libqcmaputils \
     libqcrilFramework \
